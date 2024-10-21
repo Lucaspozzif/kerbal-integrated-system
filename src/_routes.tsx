69f0e50165a587fddb593a97b91f581830e97e3e
Lucas Pozzi <lucaspozzif20@gmail.com>
2024-10-20 22:07:05 -0300

    new routes added

--- src/_routes.tsx
+++ src/_routes.tsx
@@ -1,3 +1,10 @@
 import { Home } from "./pages/home/home";
+import { ModulesForm } from "./pages/modules/form/modules-form";
+import { Modules } from "./pages/modules/modules";
 
-export const routes = [{ path: "/main_window", element: <Home /> }];
+export const routes = [
+  { path: "/main_window", element: <Home /> },
+
+  { path: "/modules", element: <Modules /> },
+  { path: "/modules/create", element: <ModulesForm /> },
+];

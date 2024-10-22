df02c5c8e99467c1ef923af351bfdc53b7ee9345
Lucas Pozzi - OneSubsea <lucaspozzif20@gmail.com>
2024-10-22 11:28:14 -0300

    firebase system created it is facing some problems, currently its not possible to use firebase since it says its offline (its not). Looking for the fix. Don't send to production at the moment

---
+++ src/system/firebase/firebase.ts
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import fbConfig from "./firebase-config.json"
+import { getFirestore } from "firebase/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+// Remember to create a new firebase-config.json file
+const firebaseConfig = {
+  apiKey: fbConfig.apiKey,
+  authDomain: fbConfig.authDomain,
+  projectId: fbConfig.projectId,
+  storageBucket: fbConfig.storageBucket,
+  messagingSenderId: fbConfig.messagingSenderId,
+  appId: fbConfig.appId,
+  measurementId: fbConfig.measurementId,
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export const db = getFirestore(app)

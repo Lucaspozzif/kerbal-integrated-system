679b098bc972c81faf47687d22ed65a2d2e470bf
Lucas Pozzi - OneSubsea <lucaspozzif20@gmail.com>
2024-10-24 11:25:47 -0300

    database class imported on database page

--- src/pages/database/database.tsx
+++ src/pages/database/database.tsx
@@ -1,11 +1,13 @@
 import { useNavigate } from "react-router-dom";
 import { LocalHeader } from "../../components/local-header/local-header";
+import { Database as Db } from "../../system/classes/database";
 
 export function Database() {
   const navigate = useNavigate();
-
+  const database = new Db();
+  
   return (
-    <div className='page'>
+    <div className="page">
       <LocalHeader
         buttons={[
           { title: "Create", onClick: () => navigate("create") },

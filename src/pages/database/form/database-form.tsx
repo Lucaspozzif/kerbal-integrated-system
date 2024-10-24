0a2f0609b7db6e00993b960bcb3f64bdc6193bf5
Lucas Pozzi - OneSubsea <lucaspozzif20@gmail.com>
2024-10-24 09:03:25 -0300

    database form real database support - Placeholder data deleted - setter updated to the real class - functions handleAdd, handleDelete, handleChange and getNextId updated to its permanent values - Markdown code updated to final format

--- src/pages/database/form/database-form.tsx
+++ src/pages/database/form/database-form.tsx
@@ -4,64 +4,78 @@ import { LocalHeader } from "../../../components/local-header/local-header";
 import { SheetButton } from "../../../components/sheet-button/sheet-button";
 import { trash } from "../../../_global";
 import { TextButton } from "../../../components/text-button/text-button";
+import { Database } from "../../../system/classes/database";
 
 export function DatabaseForm() {
-  const [data, setData] = useState<{ [key: string]: string }>({
-    "0001": "Teste",
-    "0002": "rte",
-  });
+  const [database, setter] = useState(new Database());
 
-  const getNextId = () => {
-    const keys = Object.keys(data);
-    const maxId = keys.length ? Math.max(...keys.map((key) => parseInt(key))) : 0;
-    return (maxId + 1).toString().padStart(4, "0");
-  };
-
-  const handleAdd = () => {
-    const newId = getNextId();
-    setData({
-      ...data,
-      [newId]: "",
-    });
+  const handleAdd = async () => {
+    const newId = await database.generateId();
+    database.set(
+      "data",
+      {
+        ...database.get("data"),
+        [newId]: "",
+      },
+      setter
+    );
   };
 
   const handleDelete = (id: string) => {
-    const newData = { ...data };
-    delete newData[id];
-    setData(newData);
+    const updatedData = { ...database.get("data") };
+    delete updatedData[id];
+    database.set("data", updatedData, setter);
   };
 
   const handleChange = (id: string, value: string) => {
-    setData({
-      ...data,
-      [id]: value,
-    });
+    database.set("data", { ...database.get("data"), [id]: value }, setter);
   };
 
   return (
-    <div className='page'>
-      <LocalHeader buttons={[]} text={""} />
-      <div className='form-tab'>
-        <div className='ft-inputs'>
-          <p className='ft-label'>Database Name:</p>
-          <TextInput size={2} />
+    <div className="page">
+      <LocalHeader
+        buttons={[{ title: "Save", onClick: () => database.upload() }]}
+        text={""}
+      />
+      <div className="form-tab">
+        <div className="ft-inputs">
+          <p className="ft-label">Database Name:</p>
+          <TextInput
+            size={2}
+            onChange={(e) => {
+              database.set("name", e.target.value, setter);
+            }}
+          />
         </div>
 
-        <div className='ft-sheet'>
-          <div className='fts-line'>
-            <TextInput readOnly={true} size={0} value='Id' />
-            <TextInput readOnly={true} size={2} value='Value' />
+        <div className="ft-sheet">
+          <div className="fts-line">
+            <TextInput readOnly={true} size={0} value="Id" />
+            <TextInput readOnly={true} size={2} value="Value" />
           </div>
 
-          {Object.entries(data).map(([id, value]) => (
-            <div className='fts-line' key={id}>
-              <TextInput readOnly={true} size={0} value={id} />
-              <TextInput size={2} value={value} onChange={(e) => handleChange(id, e.target.value)} />
-              <SheetButton src={trash} onClick={() => handleDelete(id)} />
-            </div>
-          ))}
-          <div className='fts-line'>
-            <TextButton onClick={handleAdd} title='add line' />
+          {database.get("data") ? (
+            Object.entries(database.get("data")).map(([id, value]: any) => (
+              <div className="fts-line" key={id}>
+                <TextInput readOnly={true} size={0} value={id} />
+                <TextInput
+                  size={2}
+                  value={value}
+                  onChange={(e) => handleChange(id, e.target.value)}
+                />
+                <SheetButton src={trash} onClick={() => handleDelete(id)} />
+              </div>
+            ))
+          ) : (
+            <></>
+          )}
+          <div className="fts-line">
+            <TextButton
+              onClick={() => {
+                if (database.get("name") !== undefined || "") handleAdd();
+              }}
+              title="add line"
+            />
           </div>
         </div>
       </div>

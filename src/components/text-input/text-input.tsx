b38d6caf548afb6973b20ddaa80bdf3a1a8fee39
Lucas Pozzi <lucaspozzif20@gmail.com>
2024-10-26 16:10:46 -0300

    text input type update now it can be selected, made to fit the spreadsheets selection

--- src/components/text-input/text-input.tsx
+++ src/components/text-input/text-input.tsx
@@ -2,11 +2,12 @@ import "./text-input.css";
 
 type TextInputType = {
   readOnly?: boolean;
+  selected?: boolean;
   size: number;
   value?: string;
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
-export function TextInput({ readOnly = false, size, value, onChange }: TextInputType) {
-  return <input className={`text-input ${readOnly ? "" : "editable"} size${size}`} type='text' value={value} readOnly={readOnly} onChange={onChange} />;
+export function TextInput({ readOnly = false, selected = false, size, value, onChange }: TextInputType) {
+  return <input className={`text-input ${selected ? "selected" : ""} ${readOnly ? "" : "editable"} size${size}`} type='text' value={value} readOnly={readOnly} onChange={onChange} />;
 }

61d8686374ce64c3b5dd6eba9e8c9d2b0e30e0a5
Lucas Pozzi <lucaspozzif20@gmail.com>
2024-10-25 23:11:56 -0300

    docs form overhauled

--- src/pages/docs/form/docs-form.tsx
+++ src/pages/docs/form/docs-form.tsx
@@ -1,78 +1,243 @@
-import { useState } from "react";
+import { useEffect, useState, version } from "react";
 import { LocalHeader } from "../../../components/local-header/local-header";
 import { TextInput } from "../../../components/text-input/text-input";
 import { IconButton } from "../../../components/icon-button/icon-button";
-import { printIcon, importIcon, exportIcon } from "../../../_global";
+import { exportIcon, edit, file, importIcon } from "../../../_global";
 import { Document } from "../../../system/classes/document";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
+import { SheetButton } from "../../../components/sheet-button/sheet-button";
+import { TextButton } from "../../../components/text-button/text-button";
+import { serverTimestamp } from "firebase/firestore";
+import { Loading } from "../../../components/loading/loading";
 
 export function DocsForm() {
-  const [tab, setTab] = useState("basic-data");
+  const [loading, setLoading] = useState(false);
   const [document, setter] = useState(new Document());
+  const [tab, setTab] = useState("basic-data");
 
-  console.log(document);
+  const [mode, setMode] = useState("display");
+  const [fileMode, setfileMode] = useState("display");
+
+  const [selectedGroup, setSelectedGroup] = useState("");
+
+  const { id } = useParams();
+  const location: any = useLocation();
+  const navigate = useNavigate();
+
+  const tempFiles = [];
+
+  useEffect(() => {
+    const fetch = async () => {
+      setLoading(true);
+      setMode(location.split("/")[1]);
+      if (id) {
+        document.set("id", id, setter);
+        await document.download();
+      }
+      setLoading(false);
+    };
+
+    fetch();
+  }, []);
 
   const tabHandler = () => {
+    const lastVersions = document.lastVersionIds();
+
     switch (tab) {
       case "basic-data":
         return (
-          <div className="form-tab">
-            <div className="ft-inputs">
-              <p className="ft-label">Id:</p>
+          <div className='form-tab'>
+            <div className='ft-inputs'>
+              <p className='ft-label'>Id:</p>
               <TextInput readOnly={true} size={0} value={document.get("id")} />
-              <p className="ft-label">Name:</p>
-              <TextInput
-                size={2}
-                value={document.get("name")}
-                onChange={(e) => document.set("name", e.target.value, setter)}
-              />
 
-              <p className="ft-label">Long:</p>
-              <TextInput
-                size={3}
-                value={document.get("long")}
-                onChange={(e) => document.set("long", e.target.value, setter)}
-              />
+              <p className='ft-label'>Name:</p>
+              <TextInput readOnly={mode != "edit"} size={2} value={document.get("name")} onChange={(e) => document.set("name", e.target.value, setter)} />
 
-              <p className="ft-label">Type:</p>
-              <TextInput
-                readOnly={true}
-                size={2}
-                value={document.get("type")}
-              />
-
-              <p className="ft-label">Status:</p>
-              <TextInput
-                readOnly={true}
-                size={2}
-                value={document.get("status")}
-              />
+              <p className='ft-label'>Long:</p>
+              <TextInput readOnly={mode != "edit"} size={3} value={document.get("long")} onChange={(e) => document.set("long", e.target.value, setter)} />
             </div>
           </div>
         );
+
       case "files":
         return (
-          <div className="form-tab">
-            <div className="ft-sheet">
-              <div className="fts-line">
-                <TextInput readOnly={true} size={1} value="Dock. Port" />
-                <TextInput readOnly={true} size={0} value="Units" />
-                <TextInput readOnly={true} size={0} value="Using" />
+          <div className='form-tab'>
+            <div className='ft-sheet'>
+              <div className='fts-line'>
+                <TextInput readOnly={true} size={0} value='Id' />
+                <TextInput readOnly={true} size={2} value='Name' />
+                <TextInput readOnly={true} size={0} value='Ver' />
+                <TextInput readOnly={true} size={2} value='Uploaded On' />
+                <TextInput readOnly={true} size={3} value='Description' />
               </div>
-              <div className="fts-line">
-                <TextInput readOnly={true} size={1} value="Dock. Port" />
-                <TextInput size={0} />
-                <TextInput size={0} />
-              </div>
+              {document.get("files").map((file: any) => {
+                return (
+                  <div className='fts-line'>
+                    <div
+                      className='fts-line'
+                      onClick={() => {
+                        setTab("file-form");
+                        setSelectedGroup(file.name);
+                        setfileMode("display");
+                      }}
+                    >
+                      <TextInput readOnly={true} size={0} value={file.id} />
+                      <TextInput readOnly={true} size={2} value={file.name} />
+                      <TextInput readOnly={true} size={0} value={file.ver} />
+                      <TextInput readOnly={true} size={2} value={file.uploaded} />
+                      <TextInput readOnly={true} size={3} value={file.description} />
+                    </div>
+                    <SheetButton
+                      src={exportIcon}
+                      onClick={async () => {
+                        setLoading(true);
+                        await document.downloadFile(file.id);
+                        setLoading(false);
+                      }}
+                    />
+                    {mode == "edit" && lastVersions.includes(file.id) ? (
+                      <SheetButton
+                        src={edit}
+                        onClick={() => {
+                          setTab("file-form");
+                          setSelectedGroup(file.groupId);
+                          setfileMode("edit");
+                        }}
+                      />
+                    ) : (
+                      <></>
+                    )}
+                  </div>
+                );
+              })}
+              {mode == "edit" ? (
+                <TextButton
+                  title='add File'
+                  onClick={() => {
+                    const tempGroup = `$${serverTimestamp()}`;
+                    setTab("file-form");
+                    setSelectedGroup(tempGroup);
+                    setfileMode("edit");
+                    const updatedFiles = document.get("files");
+                    updatedFiles.push({
+                      id: `$${serverTimestamp()}`,
+                      groupId: tempGroup,
+                      name: selectedGroup,
+                      description: "",
+                      version: "00",
+                      uploaded: "",
+                    });
+
+                    document.set("files", updatedFiles, setter);
+                  }}
+                />
+              ) : (
+                <></>
+              )}
             </div>
           </div>
         );
+
+      case "file-form":
+        const files: any[] = document.get("files").filter((file: any) => file.group == selectedGroup);
+        const lastVersion: string = files
+          .reduce((max: any, file: any) => (parseInt(file.version) > parseInt(max.version) ? file : max), files[0])
+          .toString()
+          .startPad(2, "0");
+
+        return (
+          <div className='form-tab'>
+            <div className='ft-inputs'>
+              <p className='ft-label'>Last Version:</p>
+              <TextInput readOnly={true} size={0} value={lastVersion} />
+
+              <p className='ft-label'>Name:</p>
+              <TextInput
+                readOnly={selectedGroup != "" || !selectedGroup.startsWith("$")}
+                size={2}
+                value={files[0].name}
+                onChange={(e) => {
+                  const updatedFiles = document.get("files").forEach((file: any) => {
+                    if (file.group.startsWith("$")) file.name = e.target.value;
+                  });
+                  document.set("files", updatedFiles, setter);
+                }}
+              />
+            </div>
+
+            <div className='ft-sheet'>
+              <div className='fts-line'>
+                <TextInput readOnly={true} size={0} value='Id' />
+                <TextInput readOnly={true} size={0} value='Ver' />
+                <TextInput readOnly={true} size={2} value='Uploaded On' />
+                <TextInput readOnly={true} size={3} value='Description' />
+              </div>
+              {files.map((file: any) => {
+                return (
+                  <div className='fts-line'>
+                    <TextInput readOnly={true} size={0} value={file.id} />
+                    <TextInput readOnly={true} size={0} value={file.ver} />
+                    <TextInput readOnly={true} size={2} value={file.uploaded} />
+                    <TextInput
+                      readOnly={!file.id.startsWith("$")}
+                      size={3}
+                      value={file.description}
+                      onChange={(e) => {
+                        const updatedFiles = document.get("files").forEach((file: any) => {
+                          if (file.id.startsWith("$")) file.description = e.target.value;
+                        });
+                        document.set("files", updatedFiles, setter);
+                      }}
+                    />
+                    {file.id.startsWith("$") ? (
+                      <SheetButton
+                        src={importIcon}
+                        onClick={() => {
+                          document.importFile(/**Placeholder */ "");
+                        }}
+                      />
+                    ) : (
+                      <SheetButton
+                        src={exportIcon}
+                        onClick={() => {
+                          document.exportFile(/**Placeholder */ "");
+                        }}
+                      />
+                    )}
+                  </div>
+                );
+              })}
+              {fileMode == "edit" && !document.get("files").some((file: any) => file.id.startsWith("$")) ? (
+                <TextButton
+                  title='add File'
+                  onClick={() => {
+                    const updatedFiles = document.get("files");
+                    updatedFiles.push({
+                      id: `$${serverTimestamp()}`,
+                      groupId: selectedGroup,
+                      name: "",
+                      description: "",
+                      version: (parseInt(lastVersion) + 1).toString().padStart(2, "0"),
+                      uploaded: "",
+                    });
+                    document.set("files", updatedFiles, setter);
+                  }}
+                />
+              ) : (
+                <></>
+              )}
+            </div>
+          </div>
+        );
+
       default:
-        return <div className="tab"></div>;
+        return <div className='tab'></div>;
     }
   };
 
   return (
-    <div className="page">
+    <div className='page'>
       <LocalHeader
         buttons={[
           {
@@ -91,19 +256,35 @@ export function DocsForm() {
             long: true,
             selected: tab == "",
             spaced: true,
-            title: "Display",
-            onClick: () => {},
+            title: mode != "display" ? "Display" : "Edit",
+            onClick: () => {
+              if (mode == "display") setMode("edit");
+              else setMode("display");
+            },
           },
           {
             long: true,
             selected: tab == "",
-            title: "Unsaved",
-            onClick: () => {},
+            title: "Save",
+            onClick: async () => {
+              setLoading(true);
+              document.get("files").forEach(async (file: any) => {
+                if (id.startsWith("$")) {
+                  file.id = await document.generateId("files", 4);
+                  file.uploaded = serverTimestamp();
+                }
+              });
+              if (!document.get("id")) {
+                document.set("id", await document.generateId("documents", 4));
+                await document.upload();
+              }
+              setLoading(false);
+            },
           },
         ]}
         text={""}
       />
-      {tabHandler()}
+      {loading ? <Loading /> : tabHandler()}
     </div>
   );
 }

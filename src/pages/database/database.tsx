a16d42f8e5ad7d5fdd6d483f23f65943f34dd76d
Lucas Pozzi <lucaspozzif20@gmail.com>
2024-10-27 12:45:09 -0300

    database to standart procedure

--- src/pages/database/database.tsx
+++ src/pages/database/database.tsx
@@ -2,34 +2,281 @@ import { useNavigate } from "react-router-dom";
 import { LocalHeader } from "../../components/local-header/local-header";
 import { Database as Db } from "../../system/classes/database";
 import { useEffect, useState } from "react";
+import { TextInput } from "../../components/text-input/text-input";
+import { TextButton } from "../../components/text-button/text-button";
 
 export function Database() {
-  const navigate = useNavigate();
   const [loading, setLoading] = useState(false);
-  const [list, setList] = useState([]);
-  const database = new Db();
+  const [searchLoading, setSearchLoading] = useState(false);
+  const [database, setter] = useState(new Db());
+  const [tab, setTab] = useState(0);
+  const [popup, setPopup] = useState("none");
 
-  
-  console.log(list)
+  const navigate = useNavigate();
+
+  const [list, setList] = useState([]);
+  const [results, setResults] = useState([]);
+  const [search, setSearch] = useState("");
+  const [safetyText, setSafetyText] = useState("");
+
+  const selectedId = database.get("name");
+
+  useEffect(() => {
+    const fetch = async () => {
+      setLoading(true);
+      if (list.length < 1) {
+        setList(await database.downloadInterval(tab * 3, tab * 3 + 3));
+      }
+      setLoading(false);
+    };
+    fetch();
+  });
+
+  const popupHandler = () => {
+    switch (popup) {
+      case "search":
+        return (
+          <div
+            className='blur'
+            onClick={() => {
+              setPopup("none");
+            }}
+          >
+            <div
+              className='popup'
+              onClick={(e) => {
+                e.stopPropagation();
+              }}
+            >
+              <LocalHeader
+                buttons={[
+                  { title: "Create", onClick: () => navigate("create") },
+                  {
+                    title: "Edit",
+                    onClick: () => {
+                      if (selectedId) {
+                        navigate(`edit/${selectedId}`);
+                      }
+                    },
+                  },
+                  {
+                    title: "Search",
+                    onClick: async () => {
+                      setSearchLoading(true);
+                      const searchResults = await database.downloadSearch(search);
+                      setResults(searchResults);
+                      setSearchLoading(false);
+                    },
+                  },
+                  {
+                    title: "Display",
+                    onClick: () => {
+                      if (selectedId) {
+                        navigate(`display/${selectedId}`);
+                      }
+                    },
+                  },
+                  {
+                    title: "Delete",
+                    onClick: () => {
+                      if (selectedId) {
+                        setPopup("delete");
+                      }
+                    },
+                  },
+                ]}
+                text={`Selected: ${database.get("name") || "none"}`}
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+              />
+              <div className='ft-sheet'>
+                <div className='fts-line'>
+                  <TextInput readOnly={true} size={2} value='Name' />
+                  <TextInput readOnly={true} size={1} value='Uploaded On' />
+                </div>
+                {results.map((file) => {
+                  const date = new Date(file.created);
+
+                  // Get day, month, and year
+                  const day = String(date.getDate()).padStart(2, "0"); // Ensure two digits
+                  const month = String(date.getMonth() + 1).padStart(2, "0"); // Months are zero-indexed
+                  const year = date.getFullYear();
+                  return (
+                    <div
+                      className='fts-line'
+                      onClick={() => {
+                        if (file.name == database.get("name")) {
+                          setter(new Db());
+                        } else {
+                          setter(new Db(file));
+                        }
+                      }}
+                    >
+                      <TextInput selected={file.name == database.get("name")} readOnly={true} size={2} value={file.name} />
+                      <TextInput selected={file.name == database.get("name")} readOnly={true} size={1} value={`${day}-${month}-${year}`} />
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          </div>
+        );
+      case "delete":
+        return (
+          <div
+            className='blur'
+            onClick={() => {
+              setPopup("none");
+              setSafetyText("");
+            }}
+          >
+            <div
+              className='popup'
+              onClick={(e) => {
+                e.stopPropagation();
+              }}
+            >
+              <div className='textbox'>
+                <p className='text'>Are you sure you want to delete this Document?</p>
+                <p className='text'>This action is PERMANENT!</p>
+              </div>
+              <div className='ft-sheet'>
+                <div className='fts-line'>
+                  <TextInput readOnly={true} size={2} value='Name' />
+                  <TextInput readOnly={true} size={1} value='Uploaded On' />
+                </div>
+                {[""].map(() => {
+                  const date = new Date(database.get("created"));
+
+                  // Get day, month, and year
+                  const day = String(date.getDate()).padStart(2, "0"); // Ensure two digits
+                  const month = String(date.getMonth() + 1).padStart(2, "0"); // Months are zero-indexed
+                  const year = date.getFullYear();
+                  return (
+                    <div className='fts-line'>
+                      <TextInput readOnly={true} size={2} value={database.get("name")} />
+                      <TextInput readOnly={true} size={1} value={`${day}-${month}-${year}`} />
+                    </div>
+                  );
+                })}
+              </div>
+              <div className='flex-textbox'>
+                <p className='ft-label'>Type The database name:</p>
+                <TextInput
+                  size={2}
+                  value={safetyText}
+                  onChange={(e) => {
+                    setSafetyText(e.target.value);
+                  }}
+                />
+              </div>
+              <div className='textbox'>
+                <TextButton
+                  title='Delete'
+                  selected={safetyText != database.get("name")}
+                  onClick={async () => {
+                    if (safetyText == database.get("name")) {
+                      setLoading(true);
+                      await database.delete();
+                      setPopup("none");
+                      setter(new Db());
+                      setList(await database.downloadInterval(tab * 3, tab * 3 + 3));
+                      setSafetyText("");
+                      setLoading(false);
+                    }
+                  }}
+                />
+              </div>
+              <div className='exit-button'>
+                <TextButton
+                  title='Cancel'
+                  onClick={() => {
+                    setPopup("none");
+                    setSafetyText("");
+                  }}
+                />
+              </div>
+            </div>
+          </div>
+        );
+      default:
+        return <></>;
+    }
+  };
 
   return (
-    <div className="page">
+    <div className='page'>
+      {popupHandler()}
       <LocalHeader
         buttons={[
           { title: "Create", onClick: () => navigate("create") },
-          { title: "Display", onClick: () => navigate("display") },
-          { title: "Search", onClick: () => navigate("search") },
+          {
+            title: "Edit",
+            onClick: () => {
+              if (selectedId) {
+                navigate(`edit/${selectedId}`);
+              }
+            },
+          },
+          {
+            title: "Display",
+            onClick: () => {
+              if (selectedId) {
+                navigate(`display/${selectedId}`);
+              }
+            },
+          },
+          { title: "Search", onClick: () => setPopup("search") },
+          {
+            title: "Delete",
+            onClick: () => {
+              if (selectedId) {
+                setPopup("delete");
+              }
+            },
+          },
         ]}
-        text={""}
+        text={`Interval: Today - ${tab * 3 + 3} Months ago | Selected: ${database.get("name") || "none"}`}
       />
-      {list.map((database) => {
-        console.log(database);
-        return (
-          <div className="listItem">
-            <p>teste</p>
-          </div>
-        );
-      })}
+      <div className='ft-sheet'>
+        <div className='fts-line'>
+          <TextInput readOnly={true} size={2} value='Name' />
+          <TextInput readOnly={true} size={1} value='Uploaded On' />
+        </div>
+        {list.map((file) => {
+          const date = new Date(file.created);
+
+          // Get day, month, and year
+          const day = String(date.getDate()).padStart(2, "0"); // Ensure two digits
+          const month = String(date.getMonth() + 1).padStart(2, "0"); // Months are zero-indexed
+          const year = date.getFullYear();
+          return (
+            <div
+              className='fts-line'
+              onClick={() => {
+                if (file.id == database.get("name")) {
+                  setter(new Db());
+                } else {
+                  setter(new Db(file));
+                }
+              }}
+            >
+              <TextInput selected={file.name == database.get("name")} readOnly={true} size={2} value={file.name} />
+              <TextInput selected={file.name == database.get("name")} readOnly={true} size={1} value={`${day}-${month}-${year}`} />
+            </div>
+          );
+        })}
+        <TextButton
+          title='load'
+          onClick={async () => {
+            setLoading(true);
+            setTab(tab + 1);
+            setList(await database.downloadInterval(0, tab * 3 + 3));
+
+            setLoading(false);
+          }}
+        />
+      </div>
     </div>
   );
 }
--- src/pages/database/form/database-form.tsx
+++ src/pages/database/form/database-form.tsx
@@ -1,82 +1,157 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { TextInput } from "../../../components/text-input/text-input";
-import { LocalHeader } from "../../../components/local-header/local-header";
+import { Database } from "../../../system/classes/database";
 import { SheetButton } from "../../../components/sheet-button/sheet-button";
 import { trash } from "../../../_global";
 import { TextButton } from "../../../components/text-button/text-button";
-import { Database } from "../../../system/classes/database";
+import { LocalHeader } from "../../../components/local-header/local-header";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 
 export function DatabaseForm() {
+  const [loading, setLoading] = useState(false);
   const [database, setter] = useState(new Database());
+  const [tab, setTab] = useState("basic-data");
 
-  const handleAdd = async () => {
-    const newId = await database.generateId();
-    database.set(
-      "data",
-      {
-        ...database.get("data"),
-        [newId]: "",
-      },
-      setter
-    );
-  };
+  const [mode, setMode] = useState("display");
 
-  const handleDelete = (id: string) => {
-    const updatedData = { ...database.get("data") };
-    delete updatedData[id];
-    database.set("data", updatedData, setter);
-  };
+  const { id } = useParams();
+  const location = useLocation();
+  const navigate = useNavigate();
 
-  const handleChange = (id: string, value: string) => {
-    database.set("data", { ...database.get("data"), [id]: value }, setter);
-  };
+  useEffect(() => {
+    const fetch = async () => {
+      setLoading(true);
+      setMode(location.pathname.split("/")[2]);
+      if (id) {
+        database.set("name", id, setter);
+        await database.download();
+        setter(database);
+      }
+      setLoading(false);
+    };
 
+    fetch();
+  }, []);
+
+  if (!database.get("data")) {
+    database.set("data", [], setter);
+  }
   return (
-    <div className="page">
+    <div className='page'>
       <LocalHeader
-        buttons={[{ title: "Save", onClick: () => database.upload() }]}
+        buttons={[
+          {
+            long: true,
+            selected: tab == "basic-data",
+            title: "Basic Data",
+            onClick: () => setTab("basic-data"),
+          },
+          {
+            long: true,
+            selected: tab == "",
+            spaced: true,
+            title: mode != "display" ? "Display" : "Edit",
+            onClick: () => {
+              if (mode == "display") setMode("edit");
+              else setMode("display");
+            },
+          },
+          {
+            long: true,
+            selected: !database.get("name") || database.get("name") == "",
+            title: "Save",
+            onClick: async () => {
+              setLoading(true);
+              if (!database.get("name") || database.get("name") == "") {
+                return;
+              }
+
+              if (!database.get("data")) {
+                database.set("data", [], setter);
+              }
+              const data = database.get("data");
+
+              // Update all files in this group with the new groupId and individual file IDs
+              for (const item of data) {
+                if (item.id.startsWith("$")) {
+                  item.id = await database.generateId(`db-${database.get("name")}`, 2); // Generate new fileId
+                }
+              }
+
+              await database.upload();
+
+              setLoading(false);
+              navigate(-1);
+            },
+          },
+        ]}
         text={""}
       />
-      <div className="form-tab">
-        <div className="ft-inputs">
-          <p className="ft-label">Database Name:</p>
+      <div className='form-tab'>
+        <div className='ft-inputs'>
+          <p className='ft-label'>Name:</p>
           <TextInput
+            readOnly={database.get("created")}
             size={2}
+            value={database.get("name")}
             onChange={(e) => {
-              database.set("name", e.target.value, setter);
+              if (database.get("created")) {
+                database.set("name", e.target.value, setter);
+              }
             }}
           />
         </div>
-
-        <div className="ft-sheet">
-          <div className="fts-line">
-            <TextInput readOnly={true} size={0} value="Id" />
-            <TextInput readOnly={true} size={2} value="Value" />
+        <div className='ft-sheet'>
+          <div className='fts-line'>
+            <TextInput readOnly={true} size={0} value='Id' />
+            <TextInput readOnly={true} size={2} value='Value' />
           </div>
-
-          {database.get("data") ? (
-            Object.entries(database.get("data")).map(([id, value]: any) => (
-              <div className="fts-line" key={id}>
-                <TextInput readOnly={true} size={0} value={id} />
+          {database.get("data").map((item: any, index: number) => {
+            return (
+              <div className='fts-line'>
+                <TextInput readOnly={true} size={0} value={item.id} />
                 <TextInput
+                  readOnly={mode == "display"}
                   size={2}
-                  value={value}
-                  onChange={(e) => handleChange(id, e.target.value)}
+                  value={item.value}
+                  onChange={(e) => {
+                    // Find the index of the file with the matching ID
+                    const index = database.get("data").findIndex((fileIn: any) => fileIn.id === item.id);
+
+                    if (index !== -1) {
+                      // Update the description of the matched file
+                      const updatedData = [...database.get("data")]; // Clone the array to avoid mutation
+                      updatedData[index].value = e.target.value;
+
+                      // Set the updated files back into the database
+                      database.set("data", updatedData, setter);
+                    }
+                  }}
                 />
-                <SheetButton src={trash} onClick={() => handleDelete(id)} />
+                <SheetButton
+                  src={trash}
+                  onClick={() => {
+                    const updatedData = [...database.get("data")].filter((_, i) => {
+                      return i !== index;
+                    });
+
+                    database.set("data", updatedData, setter);
+                  }}
+                />
               </div>
-            ))
-          ) : (
-            <></>
-          )}
-          <div className="fts-line">
-            <TextButton
-              onClick={() => {
-                if (database.get("name") !== undefined || "") handleAdd();
-              }}
-              title="add line"
-            />
-          </div>
+            );
+          })}
+          <TextButton
+            title='add Item'
+            onClick={() => {
+              const updatedData = [...database.get("data")];
+              updatedData.push({
+                id: `$${Date.now()}`,
+                value: "",
+              });
+              database.set("data", updatedData, setter);
+            }}
+          />
         </div>
       </div>
     </div>

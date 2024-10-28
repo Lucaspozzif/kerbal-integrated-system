6414c670be40396990aeb5065f93b8e502995fb7
Lucas Pozzi - OneSubsea <lucaspozzif20@gmail.com>
2024-10-28 14:25:56 -0300

    routes uptaded - new paths added to the science tab - reordered documents and database to keep the development order

--- src/_routes.tsx
+++ src/_routes.tsx
@@ -2,23 +2,27 @@ import { Database } from "./pages/database/database";
 import { DatabaseForm } from "./pages/database/form/database-form";
 import { Docs } from "./pages/docs/docs";
 import { DocsForm } from "./pages/docs/form/docs-form";
+import { Experiment } from "./pages/experiment/experiment";
+import { ExperimentForm } from "./pages/experiment/form/experiment-form";
 import { Home } from "./pages/home/home";
 import { ModulesForm } from "./pages/modules/form/modules-form";
 import { Modules } from "./pages/modules/modules";
 
 export const routes = [
-  { path: "/main_window", element: <Home /> },
-
-  { path: "/modules", element: <Modules /> },
-  { path: "/modules/create", element: <ModulesForm /> },
-
-  { path: "/database", element: <Database /> },
-  { path: "/database/create", element: <DatabaseForm /> },
+  { path: "/", element: <Home /> },
 
   { path: "/documents", element: <Docs /> },
   { path: "/documents/create/", element: <DocsForm /> },
   { path: "/documents/edit/:id", element: <DocsForm /> },
   { path: "/documents/display/:id", element: <DocsForm /> },
-  { path: "/documents/search/", element: <DocsForm /> },
-  { path: "/documents/delete/:id", element: <DocsForm /> },
+
+  { path: "/database", element: <Database /> },
+  { path: "/database/create", element: <DatabaseForm /> },
+  { path: "/database/edit/:id", element: <DatabaseForm /> },
+  { path: "/database/display/:id", element: <DatabaseForm /> },
+
+  { path: "science", element: <Experiment /> },
+  { path: "science/create", element: <ExperimentForm /> },
+  { path: "science/edit/:id", element: <ExperimentForm /> },
+  { path: "science/display/:id", element: <ExperimentForm /> },
 ];
